93bf15d134eb568b6863538d2dfcd4b8c7e04e30
KullTC <svenwitteveen@gmail.com>
2015-11-03 14:53:16 +0100

    S61.41_refactor_account_hours_tab

--- Lib/Data/HoursData.php
+++ Lib/Data/HoursData.php
@@ -1,8 +1,21 @@
 <?php
 
+app::uses('Data', 'CakePHPUtil.Lib/Data/');
+
 class HoursData extends Data {
 
-/*
+	const POSITIVE = 1;
+
+	const NEUTRAL = 2;
+
+	const NEGATIVE = 3;
+
+/**
+ * @var null indicates the nature of this number (e.g. positive for good, negative for bad, etc.)
+ */
+	protected $_sign = null;
+
+/**
  * @var string $_text
  */
 	protected $_text = null;
@@ -26,5 +39,21 @@ class HoursData extends Data {
  */
 	public function __toString() {
 		return $this->_text;
+	}
+
+/**
+ * @param $sign
+ */
+	public function setSign($sign) {
+		if ($sign == self::POSITIVE || $sign == self::NEGATIVE || $sign == self::NEUTRAL) {
+			$this->_sign = $sign;
+		}
+	}
+
+/**
+ * @return int
+ */
+	public function getSign() {
+		return isset($this->_sign) ? $this->_sign : self::NEUTRAL;
 	}
 }
