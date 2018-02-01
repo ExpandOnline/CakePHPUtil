3d021182362735f427f38459cd662e60942136cc
pvaass <pvaassens@expandonline.nl>
2018-02-01 16:02:11 +0100

    S120.11 Allow custom header

--- Controller/Component/CorsComponent.php
+++ Controller/Component/CorsComponent.php
@@ -9,7 +9,7 @@ class CorsComponent extends Component {
 
 		if($controller->request->is("options")){
 			$controller->response->header('Access-Control-Allow-Methods','POST, PUT, GET, DELETE, PATCH, OPTIONS');
-			$controller->response->header('Access-Control-Allow-Headers','Content-Type, Authorization');
+			$controller->response->header('Access-Control-Allow-Headers','Content-Type, Authorization, x-custom');
 			$controller->response->send();
 
 			$this->_stop();
